20116a170a426028d7f6ae75a265d5e440c9c727
ajyong <aaron@liftinteractive.com>
2015-05-30 23:00:25 -0600

    Change Ionic app name, add services.js

--- app/corstutorial/www/js/app.js
+++ app/corstutorial/www/js/app.js
@@ -1,10 +1,10 @@
-// Ionic Starter App
+// CORS Tutorial Ionic App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'corstutorial' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'corstutorial.controllers' is found in controllers.js
+angular.module('corstutorial', ['ionic', 'corstutorial.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
---
+++ app/corstutorial/www/js/services.js
@@ -0,0 +1,5 @@
+angular.module('corstutorial.services', [])
+
+.factory('authentication', function() {
+  
+});
